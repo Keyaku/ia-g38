e3cb3356e40cdafb11f11bc5b70105a321135db1
Lucia Lisboa <lucy.hf.c@hotmail.com>
2017-10-05 17:24:52 +0100

    Alterações por causa da convenção

--- part1/src/same_game.py
+++ part1/src/same_game.py
@@ -1,25 +1,38 @@
 from search import Problem
 
+
 # TAI color
 # sem cor = 0
 # com cor > 0
 def get_no_color():
 	return 0
+
+
 def no_color(c):
-	return c==0
+	return c == 0
+
+
 def color(c):
 	return c > 0
 
+
+# -------------------------------------------------------------------------
 
 # TAI pos
 # Tuplo (l, c)
 def make_pos(l, c):
 	return (l, c)
+
+
 def pos_l(pos):
 	return pos[0]
+
+
 def pos_c(pos):
 	return pos[1]
 
+
+# ----------------------------------------------------------------------------
 
 # TAI group
 # Lista de pecas adjacentes
@@ -27,24 +40,30 @@ def pos_c(pos):
 def make_group(board, l, c):
 	return group_find_adj(board, board[l][c], l, c, [])
 
+
 def group_find_adj(board, cor, l, c, adj):
+
 	if board_is_pos(board, l, c) and cor == board[l][c]:
+
 		pos = make_pos(l, c)
+
 		if pos not in adj:
 			adj += [pos]
 
 			# deCORando a COR. Get it?
 			cor = board[l][c]
-			board[l][c] = get_no_color() # marcamos como visitado
+			board[l][c] = get_no_color()  # marcamos como visitado
 
 			# Verificando pela ordem: sup, inf, esq, dir
-			adj = group_find_adj(board, cor, l+1, c, adj)
-			adj = group_find_adj(board, cor, l-1, c, adj)
-			adj = group_find_adj(board, cor, l, c+1, adj)
-			adj = group_find_adj(board, cor, l, c-1, adj)
+			adj = group_find_adj(board, cor, l + 1, c, adj)
+			adj = group_find_adj(board, cor, l - 1, c, adj)
+			adj = group_find_adj(board, cor, l, c + 1, adj)
+			adj = group_find_adj(board, cor, l, c - 1, adj)
 
 	return adj
 
+
+# --------------------------------------------------------------------------------
 
 # TAI board
 # Lista de listas de color
@@ -54,26 +73,32 @@ def board_clone(board):
 		new_board += [list(line)]
 	return new_board
 
+
 def board_print(board):
 	for line in board:
 		print(line)
 
+
 def board_is_pos(board, l, c):
 	return 0 <= l < len(board) and 0 <= c < len(board[0])
 
+
 def board_find_groups(board):
 	new_board = board_clone(board)
-	lista_de_grupos = [] # tem que ter tamanho de numero de cores
+	lista_de_grupos = []  # tem que ter tamanho de numero de cores
 
 	b_range = range(len(new_board))
+
 	for l in b_range:
 		for c in b_range:
+
 			if color(new_board[l][c]):
 				lista_de_grupos += [make_group(new_board, l, c)]
 			else:
 				continue
 
 	return lista_de_grupos
+
 
 def board_remove_group(board, group):
 	new_board = board_clone(board)
@@ -86,6 +111,8 @@ def board_remove_group(board, group):
 
 	return new_board
 
+
+# ---------------------------------------------------------------------------
 
 # TAI sg_state
 # Classe que contem configuracao de uma board
@@ -101,12 +128,16 @@ class same_game(Problem):
 
 	def actions(self, state):
 		pass
+
 	def result(self, state, action):
 		pass
+
 	def goal_test(self, state):
 		pass
+
 	def path_cost(self, c, state1, action, state2):
 		pass
+
 	def h(self, node):
 		"""Needed for informed search."""
 		pass
