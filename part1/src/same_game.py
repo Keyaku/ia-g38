a93a69fadba0543743dc50ea1a86a98ab48a7902
Lucia Lisboa <lucy.hf.c@hotmail.com>
2017-10-08 18:48:05 +0100

    Adicionado GoalAgent que vai fazer override dos métodos não implementados e tentativas de fazer o same_game. Algumas coisas coloquei em comentário

--- part1/src/same_game.py
+++ part1/src/same_game.py
@@ -1,4 +1,4 @@
-from search import Problem
+from search import Problem, SimpleProblemSolvingAgentProgram, astar_search
 
 
 # TAI color
@@ -42,7 +42,6 @@ def make_group(board, l, c):
 
 
 def group_find_adj(board, cor, l, c, adj):
-
 	if board_is_pos(board, l, c) and cor == board[l][c]:
 
 		pos = make_pos(l, c)
@@ -119,25 +118,72 @@ def board_remove_group(board, group):
 # TODO
 
 # TAI same_game
+# Herda da class Problem do ficheiro search.py
 class same_game(Problem):
 	"""Models a Same Game problem as a satisfaction problem.
 	A solution cannot have pieces left on the board."""
 
+	#Recebe o estado inicial = board e o problem recebia um goal state tambem
+	#Adicionei um goal state tambem
 	def __init__(self, board):
+		#self.initial = board
+		#self.goal = []
 		pass
 
 	def actions(self, state):
-		pass
+		"""Return the actions that can be executed in the given
+		state. The result would typically be a list, but if there are
+		many actions, consider yielding them one at a time in an
+		iterator, rather than building them all at once."""
+		all_actions = board_find_groups(state)
+		#delete the group of pieces with only one piece
+		for group in all_actions:
+			pass
 
 	def result(self, state, action):
+		"""Return the state that results from executing the given
+		action in the given state. The action must be one of
+		self.actions(state)."""
+
 		pass
 
 	def goal_test(self, state):
+		"""Return True if the state is a goal. The default method compares the
+		state to self.goal or checks for state in self.goal if it is a
+		list, as specified in the constructor. Override this method if
+		checking against a single self.goal is not enough."""
+		#if self.goal:
+			#return True
 		pass
 
 	def path_cost(self, c, state1, action, state2):
+		"""Return the cost of a solution path that arrives at state2 from
+		state1 via action, assuming cost c to get up to state1. If the problem
+		is such that the path doesn't matter, this function will only look at
+		state2.  If the path does matter, it will consider c and maybe state1
+		and action. The default method costs 1 for every step in the path."""
+		#return c+1
 		pass
 
 	def h(self, node):
 		"""Needed for informed search."""
 		pass
+
+
+# -----------------------------------------------------------------------------------
+
+class GoalSolvingAgentProgram(SimpleProblemSolvingAgentProgram):
+	"""Abstract framework for a problem-solving agent. [Figure 3.1]"""
+
+	def update_state(self, percept):
+		raise NotImplementedError
+
+	def formulate_goal(self, state):
+
+
+	def formulate_problem(self, state, goal):
+		raise NotImplementedError
+
+	def search(self, problem):
+		same_game = same_game()
+		astar_search(problem, same_game.h())
