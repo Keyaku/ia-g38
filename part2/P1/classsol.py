759d8c4ae96241690f46faf5d1748bd83ccc7a6f
Keyaku <antonio.luis.sarmento@gmail.com>
2017-11-30 16:26:36 +0000

    Implemented experimental features
    And a training algorithm

--- part2/P1/classsol.py
+++ part2/P1/classsol.py
@@ -8,28 +8,57 @@ import timeit
 
 from sklearn.model_selection import cross_val_score
 
+import re
+
+def count_vowels(word):
+	vowels = "aáàâãeéèêiíîoóõuú"
+	count = 0
+	for c in vowels:
+		count += word.count(c)
+	return count
+
+def count_consonants(word):
+	count = len(word) - count_vowels(word)
+	return count
+
+def count_accent_symbols(word):
+	count = len(re.findall('^[a-zA-Z_]+$', word))
+	return count
+
+def count_repeated_characters(word):
+	counted = []
+	count = 0
+	for c in word:
+		if c not in counted:
+			counted += [c]
+	return len(counted)
+
 def features(X):
 
 	F = np.zeros((len(X),5))
 	for x in range(0,len(X)):
 		F[x,0] = len(X[x]) # length of the word
-		F[x,1] = # TODO
-		F[x,2] = # TODO
-		F[x,3] = # TODO
-		F[x,4] = # TODO
+		F[x,1] = count_vowels(X[x])     # number of vowels
+		F[x,2] = count_consonants(X[x]) # number of consonants
+		F[x,3] = count_accent_symbols(X[x]) # number of accented characters
+		F[x,4] = count_repeated_characters(X[x]) # number of repeated characters
 
 	return F
 
 def mytraining(f,Y):
-	# TODO
+	clf = neighbors.KNeighborsClassifier() # TODO: use mytrainingaux()
+	clf = clf.fit(f, Y)
 	return clf
 
+
 def mytrainingaux(f,Y,par):
-	# TODO
+	# TODO: use different sklearn algorithms (neighbors, datasets, tree, linear_model)
+	# neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+	# datasets.????
+	# tree.DecisionTreeClassifier(min_samples_split=min_samples_split)
+	# linear_model.RidgeClassifier(alpha)
 	return clf
 
 def myprediction(f, reg):
 	Ypred = reg.predict(f)
-	# TODO
-
 	return Ypred
