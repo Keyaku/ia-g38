7a1186b4da916046521b66b582eb9cc52d0a24e3
Lucia Lisboa <lucy.hf.c@hotmail.com>
2017-12-01 21:31:13 +0000

    Adicionado kernel chi2.

--- part2/P2/regsol.py
+++ part2/P2/regsol.py
@@ -12,6 +12,9 @@ def mytraining(X, Y):
 
 	par_kernel = ['laplacian', 'polynomial', 'rbf', 'cosine']
 
+	if allPositive(X, Y ):
+		par_kernel +=['chi2']
+
 
 	reg = None
 
@@ -32,8 +35,30 @@ def mytraining(X, Y):
 	reg.fit(X, Y)
 	return reg
 
+#----------------------------------------------------------------------------------------------------------------------#
 
 def myprediction(X, reg):
 	Ypred = reg.predict(X)
 
 	return Ypred
+
+
+#----------------------------------------------------------------------------------------------------------------------#
+def allPositive(X, Y):
+
+	return not (any(x <= 0 for x in X) or any(y <= 0 for y in Y))
+
+#----------------------------------------------------------------------------------------------------------------------#
+
+#Important kernel factors:
+
+	#Chi2
+	#Computes the exponential chi-squared kernel X and Y.
+	#The chi-squared kernel is computed between each pair of rows in X and Y. X and Y have to be non-negative.
+	# This kernel is most commonly applied to histograms.
+
+	#Sigmoid
+	# The function sigmoid_kernel computes the sigmoid kernel between two vectors.
+	# The sigmoid kernel is also known as hyperbolic tangent, or Multilayer Perceptron
+	# (because, in the neural network field, it is often used as neuron activation function).
+
