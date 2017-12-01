793f1177f917806ca7c539e5e57dd609ee1833df
Keyaku <antonio.luis.sarmento@gmail.com>
2017-12-01 16:40:30 +0000

    P1: Normalizing words + other features
    Changes:
    - Added alphabetical character counting (ignores digits)
    - Used better Classifier (and followed assignment rules)
    - Fixed retrieval of word length (by normalizing it first)
    - Improved vowel counting
    - Improved character occurence counting
    - Improved special character (~= accented words) counting
    - Removed consonant counting (useless)

--- part2/P1/classsol.py
+++ part2/P1/classsol.py
@@ -9,44 +9,65 @@ import timeit
 from sklearn.model_selection import cross_val_score
 
 import re
+import unicodedata
+
+# Auxiliary functions
+def normalize(word):
+	return str(unicodedata.normalize('NFKD', word).encode('ascii', 'ignore'))
+
+# Features
+def count_characters(word):
+	return len(normalize(word))
 
 def count_vowels(word):
-	vowels = "aáàâãeéèêiíîoóõuú"
+	vowels = "aeiouy"
+	nword = normalize(word)
 	count = 0
-	for c in vowels:
-		count += word.count(c)
-	return count
 
-def count_consonants(word):
-	count = len(word) - count_vowels(word)
+	for c in vowels:
+		count += nword.count(c)
+	
 	return count
 
 def count_accent_symbols(word):
-	count = len(re.findall('^[a-zA-Z_]+$', word))
-	return count
+	symbols = re.findall('[^\w]', word, re.A)
 
-def count_repeated_characters(word):
+	while '\n' in symbols:
+		symbols.remove('\n')
+
+	return len(symbols)
+
+def count_character_occurrences(word):
 	counted = []
-	count = 0
+
 	for c in word:
-		if c not in counted:
+		if c not in counted and word.count(c) > 1:
 			counted += [c]
+
 	return len(counted)
+
+def count_alpha(word):
+	count = len(word)
+	digits = re.findall(r'\d+', word)
+	if len(digits) > 0:
+		count -= len(digits[0])
+	return count
 
 def features(X):
 
 	F = np.zeros((len(X),5))
 	for x in range(0,len(X)):
-		F[x,0] = len(X[x]) # length of the word
-		F[x,1] = count_vowels(X[x])     # number of vowels
-		F[x,2] = count_consonants(X[x]) # number of consonants
-		F[x,3] = count_accent_symbols(X[x]) # number of accented characters
-		F[x,4] = count_repeated_characters(X[x]) # number of repeated characters
+		F[x,0] = count_characters(X[x]) # length of the word, lowers by ~0.04 in wordsclass.npy, ~0.01 in wordsclass2.npy
+		F[x,1] = count_accent_symbols(X[x]) # accented words, lowers by ~0.032
+		F[x,2] = count_vowels(X[x]) # vowels, lowers ~0.002
+		F[x,3] = count_character_occurrences(X[x]) # repeated characters, lowers by ~0.015
+		F[x,4] = count_alpha(X[x])
 
 	return F
 
+# Training functions
 def mytraining(f,Y):
-	clf = neighbors.KNeighborsClassifier() # TODO: use mytrainingaux()
+	clf = tree.DecisionTreeClassifier(min_samples_split=8) # TODO: use mytrainingaux()
 	clf = clf.fit(f, Y)
 	return clf
 
@@ -54,11 +75,19 @@ def mytraining(f,Y):
 def mytrainingaux(f,Y,par):
 	# TODO: use different sklearn algorithms (neighbors, datasets, tree, linear_model)
 	# neighbors.KNeighborsClassifier(n_neighbors, weights=weights)
+	# n_neighbors= 2 or 3
+	# weights = 'distance'
+	# weights = 'uniform'
+
 	# datasets.????
+
 	# tree.DecisionTreeClassifier(min_samples_split=min_samples_split)
+	# min_samples_split= 8
+
 	# linear_model.RidgeClassifier(alpha)
 	return clf
 
+# Prediction function
 def myprediction(f, reg):
 	Ypred = reg.predict(f)
 	return Ypred
