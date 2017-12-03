8de03b4d07233f84c65babc2986bf7e788710b2d
Lucia Lisboa <lucy.hf.c@hotmail.com>
2017-12-03 17:05:22 +0000

    Adicionados comentários no P3 e algumas linhas do enunciado que faltavam.

--- part2/P3/RLsol.py
+++ part2/P3/RLsol.py
@@ -8,10 +8,15 @@ Created on Mon Oct 16 20:31:54 2017
 """
 import numpy as np
 
+# Para calcular a trajetoria e' necessario uma politica
+
 def Q2pol(Q, eta=5):
 	# TODO
-	return
+	return pol
 
+
+# Para isso implementar o algoritmo Q-learning que a partir de uma trajetoria
+# recebida calcule os valores Q para cada accao
 class myRL:
 
 	def __init__(self, nS, nA, gamma):
@@ -22,5 +27,8 @@ class myRL:
 
 	def traces2Q(self, trace):
 		# TODO: implementar esta funcao
+		self.Q = np.zeros((self.nS, self.nA))
 
 		return self.Q
+
+#J, trajlearn = fmdp.runPolicy(4, 5, Q2pol(Q))
