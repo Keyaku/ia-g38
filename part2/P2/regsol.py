56221882fc121ece1332eeb31707bc06b5289559
Lucia Lisboa <lucy.hf.c@hotmail.com>
2017-12-04 18:56:51 +0000

    Valor mínimo -1 -> None no regsol.py

--- part2/P2/regsol.py
+++ part2/P2/regsol.py
@@ -19,15 +19,14 @@ def mytraining(X, Y):
 	reg = None
 
 	lowest_idx = 0
-	lowest_acc = -1
+	lowest_acc = None
 
 	for idx,kernel in enumerate(par_kernel):
 		reg = KernelRidge(kernel=kernel, alpha = 0.1, gamma = 0.1)
 		scores = cross_val_score(reg, X, Y, cv=5,  scoring = 'neg_mean_squared_error')
 		accuracy = -scores.mean()
-		scores = scores.std()
 
-		if lowest_acc == -1 or accuracy < lowest_acc:
+		if lowest_acc == None or accuracy < lowest_acc:
 			lowest_acc = accuracy
 			lowest_idx = idx
 
