fd147e3f33050ebd3063c62401c4d3b7e500fcd3
Lucia Lisboa <lucy.hf.c@hotmail.com>
2017-12-07 17:05:42 +0000

    Bug fixed no P3 e limpeza de comments

--- part2/P3/RLsol.py
+++ part2/P3/RLsol.py
@@ -19,23 +19,6 @@ def Q2pol(Q, eta=5):
 
 	return pol
 
-# Action Selection Policies
-
-# As mentioned above, there are three common policies used for action selection.
-#  The aim of these policies is to balance the trade-off between exploitation and exploration,
-#  by not always exploiting what has been learnt so far.
-# -greedy - most of the time the action with the highest estimated reward is chosen, called the greediest action.
-#  Every once in a while, say with a small probability , an action is selected at random. The action is selected uniformly,
-#  independant of the action-value estimates. This method ensures that if enough trials are done,
-#  each action will be tried an infinite number of times, thus ensuring optimal actions are discovered.
-# -soft - very similar to  -greedy. The best action is selected with probability 1 - and the rest of the time a random action
-#  is chosen uniformly.
-# -softmax - one drawback of -greedy and -soft is that they select random actions uniformly.
-#  The worst possible action is just as likely to be selected as the second best.
-#  Softmax remedies this by assigning a rank or weight to each of the actions, according to their action-value estimate.
-#  A random action is selected with regards to the weight associated with each action,
-#  meaning the worst actions are unlikely to be chosen. This is a good approach to take where the worst actions are very unfavourable.
-
 
 # Para isso implementar o algoritmo Q-learning que a partir de uma trajetoria
 # recebida calcule os valores Q para cada
@@ -55,7 +38,7 @@ class myRL:
 		self.Q = np.zeros((self.nS, self.nA))
 		tempQ  = np.zeros((self.nS, self.nA))
 
-		alpha = 1
+		alpha = 0.1
 		#Vai convergir para um numero
 		while True:
 			for ele in trace:
