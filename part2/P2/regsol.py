96647608addef90f015d8efbcdcb17cd588b679a
Lucia Lisboa <lucy.hf.c@hotmail.com>
2017-12-07 11:43:01 +0000

    Fix por causa do IDE

--- part2/P2/regsol.py
+++ part2/P2/regsol.py
@@ -12,34 +12,33 @@ warnings.filterwarnings("ignore")
 
 def mytraining(X, Y):
 
-	#Declaracao dos parametros
+	# Declaracao dos parametros
 	range = [1.0, 0.8, 0.6, 0.4, 0.2, 0.1]
 
 	tuned_parameters = [
 		{'kernel': ['linear']},
-		{'kernel': ['rbf'], 'alpha' :  range, 'gamma' : range},
-		{'kernel' : ['polynomial'], 'alpha' : range, 'gamma' : range},
-		{'kernel' : ['cosine']}
+		{'kernel': ['rbf'], 'alpha': range, 'gamma': range},
+		{'kernel': ['polynomial'], 'alpha': range, 'gamma': range},
+		{'kernel': ['cosine']}
 	]
 
 	"""tuned_parameters = [
 		{'kernel' : ['rbf'], 'gamma' : range, 'C': [1, 10, 100, 1000]}
 	]"""
 
-
-	#Metodos a serem utilizados
-	#reg = SVR()
+	# Metodos a serem utilizados
+	# reg = SVR()
 	reg = KernelRidge()
 
-	#Fit do metodo
+	# Fit do metodo
 	reg.fit(X, Y)
 
-	#Aplicacao da validacao cruzada
+	# Aplicacao da validacao cruzada
 	clf = GridSearchCV(KernelRidge(), tuned_parameters, cv=5, scoring='neg_mean_squared_error')
-	#clf = GridSearchCV(SVR(), tuned_parameters, cv=5, scoring='neg_mean_squared_error')
+	# clf = GridSearchCV(SVR(), tuned_parameters, cv=5, scoring='neg_mean_squared_error')
 	clf.fit(X, Y)
 
-	#Buscar o melhor estimador para a nossa regressao
+	# Buscar o melhor estimador para a nossa regressao
 	reg = clf.best_estimator_
 
 	return reg
@@ -52,4 +51,3 @@ def myprediction(X, reg):
 	return Ypred
 
 # ----------------------------------------------------------------------------------------------------------------------#
-
