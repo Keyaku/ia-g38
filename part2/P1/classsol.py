a606d5acc90981d5c293ecdcd9bc81291a0e1631
Keyaku <antonio.luis.sarmento@gmail.com>
2017-12-07 18:49:38 +0000

    P1: Code cleanup
    Changes:
    - Added comments to clear some concepts
    - Fixed features to take into account lowercase words
    - Removed unused feature
    - Removed unused packages
    - Removed test code
    - Miscellaneous fixes

--- part2/P1/classsol.py
+++ part2/P1/classsol.py
@@ -1,38 +1,44 @@
 # 84738 Lucia Lisboa - 77906 Antonio Sarmento - 38
 
 import numpy as np
-from sklearn import neighbors, datasets, tree, linear_model
-
-from sklearn.externals import joblib
-import timeit
-
-from sklearn.model_selection import cross_val_score
+from sklearn import tree
 
 import unicodedata
 
 
-# Auxiliary content
-vowels = "aeiou"
+#########################
+### Auxiliary content ###
+#########################
+
+# String normalization: transforms a Unicode string's characters to their closest ASCII counterparts
 def normalize(string):
 	return str(unicodedata.normalize('NFKD', string).encode('ascii', 'ignore'), 'utf-8')
 
 
+# Removes artifacts from a string. In this case, only removes linefeed
 def remove_artifacts(string):
 	return string.strip('\n')
 
 
-# Features
+################
+### Features ###
+################
+
+# Returns the length of a word.
 def count_characters(word):
-	return len(normalize(word))
+	return len(word)
 
 
+# Returns the number of vowels in a (normalized) word.
 def count_vowels(word):
+	vowels = "aeiou"
 	nword = normalize(word)
 	count = len([c for c in nword if c in vowels])
 
 	return count
 
 
+# Returns the number of repeated characters in a word.
 def count_repeated_characters(word):
 	counted = []
 
@@ -43,6 +49,7 @@ def count_repeated_characters(word):
 	return len(counted)
 
 
+# Returns the sum of all characters, where a = 0, b = 1, ..., z = 25
 def sum_characters(word):
 	count = 0
 
@@ -52,31 +59,18 @@ def sum_characters(word):
 	return count
 
 
-def order(word): # weights each character according to its position
+# Returns the "weight" of a word, meaning: it'll sum all the characters according to their index
+# in that word.
+def order(word):
 	count = 0
 
 	for i,c in enumerate(word):
-		count += (ord(c) - ord('a')) * (i+1)
+		count += (ord(c) - ord('a')) * i
 
 	return count
 
 
-# FIXME: new features
-def count_consonant_vowel_pair(word):
-	count = 0
-
-	for i in range(1, len(word)):
-		c1 = word[i-1]
-		c2 = word[i]
-
-		if c1 not in vowels and c2 in vowels:
-			count += 1
-		elif c1 in vowels and c2 not in vowels:
-			count -= 1
-
-	return count
-
-
+# Assembles all our features and executes them on each word given via the list X.
 def features(X):
 	feature_array = [
 		count_characters,
@@ -84,33 +78,31 @@ def features(X):
 		count_repeated_characters,
 		sum_characters,
 		order
-		#count_consonant_vowel_pair 
 	]
 
 	F = np.zeros((len(X),len(feature_array)))
 
 	for x in range(len(X)):
-		word = remove_artifacts(X[x])
+		word = remove_artifacts(X[x]).lower()
+
 		for i in range(len(feature_array)):
 			feature = feature_array[i]
 			F[x,i] = feature(word)
 
 	return F
 
+##########################
+### Training functions ###
+##########################
 
-# Training functions
+# Uses the appropriate Classifier method to process words.
 def mytraining(f,Y):
 	clf = tree.DecisionTreeClassifier(min_samples_split=2) #min_samples_split=8
-	#clf = neighbors.KNeighborsClassifier(n_neighbors = 2, weights='distance')
-	#clf = neighbors.KNeighborsClassifier(n_neighbors = 2, weights='uniform')
-	#clf = neighbors.KNeighborsClassifier(n_neighbors = 3, weights='distance')
-	#clf = neighbors.KNeighborsClassifier(n_neighbors = 3, weights='uniform')
-	#clf = linear_model.RidgeClassifier(alpha = 0.1)
 	clf = clf.fit(f, Y)
 	return clf
 
 
-# Prediction function
+# Simply calls the prediction logic from our training method.
 def myprediction(f, clf):
 	Ypred = clf.predict(f)
 	return Ypred
